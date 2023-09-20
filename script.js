91bbfc7edfc370f041c3dc5f42f84a2008ff607f
tatianaxomikova <xomikova00@mail.ru>
2023-09-20 20:31:44 +0300

    Создание файла JS

---
+++ script.js
@@ -0,0 +1,41 @@
+/*let a = 10;
+alert (a);*/
+
+// let a = 20;
+// alert (a);
+
+// let ageIphone = "Год выпуска первого iPhone 2007.";
+// alert (ageIphone);
+
+// let nameСreator = "Создатель языка JavaScript Брендан Айком.";
+// alert (nameСreator);
+
+// let b = 10;
+// let c = 2;
+// let result = b+c;
+// alert (result);
+
+// let b = 10;
+// let c = 2;
+// let result = b-c;
+// alert (result);
+
+// let b = 10;
+// let c = 2;
+// let result = b*c;
+// alert (result);
+
+// let b = 10;
+// let c = 2;
+// let result = b/c;
+// alert (result);
+
+// let b = 10;
+// let c = 2;
+// let result = b-c;
+// alert (result);
+
+let a = 2;
+let b = 5;
+let result = a ** b;
+alert (result);
