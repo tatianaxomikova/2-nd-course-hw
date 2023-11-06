ad081e5e8f6afe63d9df996f07203574fa60b4b2
tatianaxomikova <xomikova00@mail.ru>
2023-11-07 00:09:23 +0300

    1-3 задачи

---
+++ scriptHW6.js
@@ -0,0 +1,20 @@
+// const numbs = [1, 5, 4, 10, 0, 3];
+// for (let i = 0; i < numbs.length; i++) {
+//     if (numbs [i] >= 10) break;
+//     console.log(numbs[i]);
+// }
+
+// const numbers = [1, 5, 4, 10, 0, 3];
+// const i = numbers.indexOf(4)
+// console.log(i)
+
+// let union = [1, 3, 5, 10, 20];
+// union = union.join(' ');
+// console.log(union);
+
+const array = []
+for(let i = 1; i <= 3; i++) {
+  array.push([]);
+}
+
+console.log (array);
